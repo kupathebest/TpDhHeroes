90f7b4749d1a56e5ddb0ea7cc0183a1fb94eeccc
kupathebest <kupa.thebest@gmail.com>
2021-06-18 14:08:11 -0300

    agrega en la app la ruta de los archivos

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const app = express();
 const port = 3030
 const path = require("path")
 
-app.get("/", (req,res) => res.send("funcionando"))
+app.get("/", (req,res) => res.sendFile(path.join(__dirname,"visual", "index.html")))
+app.get("/babbage", (req,res) => res.sendFile(path.join(__dirname,"visual", "babbage.html")))
+app.get("/berners-lee", (req,res) => res.sendFile(path.join(__dirname,"visual", "berners-lee.html")))
+app.get("/clarke", (req,res) => res.sendFile(path.join(__dirname,"visual", "clarke.html")))
+app.get("/hamilton", (req,res) => res.sendFile(path.join(__dirname,"visual", "hamilton.html")))
+app.get("/hopper", (req,res) => res.sendFile(path.join(__dirname,"visual", "hopper.html")))
+app.get("/lovelace", (req,res) => res.sendFile(path.join(__dirname,"visual", "lovelace.html")))
+app.get("/turing", (req,res) => res.sendFile(path.join(__dirname,"visual", "turing.html")))
 
 app.listen(port, () => console.log(`el servidor esta corriendo en http://localhost:${port}`))
