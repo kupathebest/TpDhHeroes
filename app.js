d2c306809dc70d2de89f2c572a92691499d02b78
kupathebest <kupa.thebest@gmail.com>
2021-06-19 16:50:46 -0300

    app finalizada

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const app = express();
-const port = 3030
-const path = require("path")
+const port = 3030;
+const path = require("path");
 
-app.get("/", (req,res) => res.sendFile(path.join(__dirname,"visual", "index.html")))
-app.get("/babbage", (req,res) => res.sendFile(path.join(__dirname,"visual", "babbage.html")))
-app.get("/berners-lee", (req,res) => res.sendFile(path.join(__dirname,"visual", "berners-lee.html")))
-app.get("/clarke", (req,res) => res.sendFile(path.join(__dirname,"visual", "clarke.html")))
-app.get("/hamilton", (req,res) => res.sendFile(path.join(__dirname,"visual", "hamilton.html")))
-app.get("/hopper", (req,res) => res.sendFile(path.join(__dirname,"visual", "hopper.html")))
-app.get("/lovelace", (req,res) => res.sendFile(path.join(__dirname,"visual", "lovelace.html")))
-app.get("/turing", (req,res) => res.sendFile(path.join(__dirname,"visual", "turing.html")))
+app.use(express.static("public"));
+
+app.get("/", (req,res) => res.sendFile(path.join(__dirname,"views", "index.html")))
+app.get("/home", (req,res) => res.sendFile(path.join(__dirname,"views", "index.html")))
+app.get("/babbage", (req,res) => res.sendFile(path.join(__dirname,"views", "babbage.html")))
+app.get("/berners-lee", (req,res) => res.sendFile(path.join(__dirname,"views", "berners-lee.html")))
+app.get("/clarke", (req,res) => res.sendFile(path.join(__dirname,"views", "clarke.html")))
+app.get("/hamilton", (req,res) => res.sendFile(path.join(__dirname,"views", "hamilton.html")))
+app.get("/hopper", (req,res) => res.sendFile(path.join(__dirname,"views", "hopper.html")))
+app.get("/lovelace", (req,res) => res.sendFile(path.join(__dirname,"views", "lovelace.html")))
+app.get("/turing", (req,res) => res.sendFile(path.join(__dirname,"views", "turing.html")))
 
 app.listen(port, () => console.log(`el servidor esta corriendo en http://localhost:${port}`))
